4debdb010d01fbdd50ee618e1f58f5066d2bb4e3
rainarit <rraina@ucsd.edu>
2019-11-13 19:17:27 -0800

    Initial Commit

---
+++ ParsyChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsyChat
+//
+//  Created by Ritik Raina on 11/13/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
